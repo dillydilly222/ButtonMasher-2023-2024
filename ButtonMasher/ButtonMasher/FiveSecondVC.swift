67b5f327fa2dfe1c7541a6d37507e7b5dd1ef7e6
dillydilly222 <137006761+dillydilly222@users.noreply.github.com>
2023-10-19 14:18:32 -0400

    Base App
    Base App outline, with buttons, timer, labels, and segue's all placed in order

---
+++ ButtonMasher/ButtonMasher/FiveSecondVC.swift
@@ -0,0 +1,56 @@
+//
+//  FiveSecondVC.swift
+//  ButtonMasher
+//
+//  Created by The Dude on 10/17/23.
+//
+
+import UIKit
+
+class FiveSecondVC: UIViewController {
+    
+   @IBOutlet weak var timeLabel: UILabel!
+    
+    var seconds = 5
+    var timer = Timer()
+    var isTimerRunning = false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func increaseTotalClicks(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func startGame(_ sender: UIButton) {
+        runTimer()
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        seconds -= 1
+        timeLabel.text = "\(seconds) seconds"
+        if (seconds <= 0) {
+            timer.invalidate()
+            self.performSegue(withIdentifier: "finalSegue", sender: self)
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ButtonMasher/ButtonMasher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ButtonMasher
+//
+//  Created by The Dude on 10/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
